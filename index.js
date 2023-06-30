ffed6b3ca2141313c9d286afc96e04b3e7f79216
Edriss572 <edriss_miakhel@yahoo.com>
2023-06-30 19:56:24 +0300

    The Assigment is done

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+  const left = parseInt(leftNumbers, 10);
+  dodger.style.left = `${left - 1}px`;
+}
+
+function moveDodgerRight() {
+  const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    dodger.style.left = `${left + 1}px`;
+}
+
+document.addEventListener("keydown", function (event) {
+  console.log("the pressed")
+  console.log(event);
+  
+  if (event.key === "ArrowLeft") {
+    moveDodgerLeft()
+  } 
+
+  else if (event.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+  else if (event.key === "ArrowUp") {
+    const bottomNumbers = dodger.style.bottom.replace("px", "");
+    const bottom = parseInt(bottomNumbers, 10);
+    dodger.style.bottom = `${bottom + 3}px`;
+  }
+  else if (event.key === "ArrowDown") {
+    const bottomNumbers = dodger.style.bottom.replace("px", "");
+    const bottom = parseInt(bottomNumbers, 10);
+    dodger.style.bottom = `${bottom - 3}px`;
+  }
+});
